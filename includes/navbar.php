2f83de94f907da910a380c4fd71983323d81694c
Dennis Kamau <dmkamau475@gmail.com>
2021-05-31 19:00:43 +0300

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,54 @@
+
+<body>
+<div>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                        aria-expanded="false" aria-controls="navbar">
+
+                </button>
+                <a class="navbar-brand" href="http://localhost/nairoshop/"> Nairoshop </a>
+                <a class="navbar-brand pull-right hidden-sm hidden-md hidden-lg" href="#open-cart"> <i
+                            class="ion-bag"></i> 7 </a>
+            </div>
+
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="http://localhost/nairoshop/">Home</a></li>
+                    <li><a href="http://localhost/nairoshop/store/">Booking & Enquiries</a></li>
+                    <li><a href="http://localhost/nairoshop/blog/">About</a></li>
+                    <li><a href="http://localhost/nairoshop/contacts/">Librarian</a></li>
+                    <li><a href="http://localhost/nairoshop/contacts/">News & Events</a></li>
+                    <li class="dropdown">
+                        <a href="http://localhost/nairoshop/store/" class="dropdown-toggle" data-toggle="dropdown"
+                           role="button"
+                           aria-haspopup="true" aria-expanded="false">
+                            More <i class="ion-android-arrow-dropdown"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="http://localhost/nairoshop/store/product.html">Product</a></li>
+                            <li><a href="http://localhost/nairoshop/about/">About Us</a></li>
+                            <li><a href="http://localhost/nairoshop/checkout/">Checkout</a></li>
+                            <li><a href="http://localhost/nairoshop/faq/">FAQ</a></li>
+                            <li><a href="http://localhost/nairoshop/gallery/">Gallery</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li class="dropdown-header">Variations</li>
+                            <li><a href="http://localhost/nairoshop/blog/item-photo.html">Article Photo</a></li>
+                            <li><a href="http://localhost/nairoshop/blog/item-video.html">Article Video</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+
+
+            <div class="search hidden-xs" data-style="hidden">
+                <div class="input">
+                    <button type="button"><i class="ion-ios-search"></i></button>
+
+                    <input type="text" name="search" value="" placeholder="Type here..."/>
+                </div>
+            </div>
+        </div><!--/.container-fluid -->
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!--Display Page Title and fetch Header file-->
+    <?php
+    $page_title = 'Home';
+    include './includes/header.php';
+    ?>
+</head>
+<body>
+
+</body>
+</html>

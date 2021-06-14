7265a9fa2ca390861b9b3002101ee10df461ab5f
Peter Thairu Muigai <ptmthairu@gmail.com>
2021-06-14 10:58:25 +0300

    major changes and roll backs
    *rolled back login system to original design with a dashboard for user assignment
    *added a dashboard and different librarian db for logins also added a forgot password page lacking db connection and funtions

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,46 +1,36 @@
 <?php
-include_once "dbconn.inc.php";
 
-if(isset($_POST["register"])){
-    
-    function validate_data($data){
-        $data = trim($data);
-        $data = htmlspecialchars($data);
-        $data = stripslashes($data);
-        return $data;
+if(isset($_POST["reg-user"])){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $pwd = $_POST["password"];
+    $cpwd = $_POST["cpassword"];
+
+    include_once "dbconn.php";
+    include_once "functions.inc.php";
+
+    if(fieldsEmpty($name, $email, $pwd, $cpwd) !== false){
+        header ("location: ../index.php?error=emptyFields");
+        exit();
     }
-
-    $studentReg = validate_data($_POST["reg-no"]);
-    $studentEmail = validate_data($_POST["email"]);
-    $studentPwd = validate_data($_POST["pwd"]);
-    $studentCpwd = validate_data($_POST["cpwd"]);
-    $studentRole = validate_data($_POST["role"]);
-
-    if(empty($studentReg) || empty($studentEmail) || empty($studentPwd) || empty($studentCpwd) || empty($studentRole)){
-    header("location: ../index.php?error=Please fill all fields");
+    if(validUsername($name) !== false){
+        header ("location: ../index.php?error=invalidUsername");
+        exit();
     }
-    else if(!preg_match("/^[a-zA-Z0-9]*$/", $studentPwd)){
-        header("location: ../index.php?error=Enter valid password");
-        
-    }else if(!filter_var($studentEmail, FILTER_VALIDATE_EMAIL)){
-        header("location: ../index.php?error=Enter valid password");
-        
-    }else if($studentPwd !==  $studentCpwd){
-        header("location: ../index.php?error=Unmatched password, try again");
-        
+    if(validEmail($email) !== false){
+        header ("location: ../index.php?error=invalidEmail");
+        exit();
     }
-    
-    $hashedpwd = md5($studentPwd);
-
-    $sql = "INSERT INTO signup_user (user_name, user_email, user_password, user_role) 
-    VALUES('$studentReg', '$studentEmail', '$hashedpwd', '$studentRole')";
-
-    $query = mysqli_query($conn, $sql);
-
-    if($query){
-        header("location:../index.php");
-    }else{
-        header("location:../index.php?error=Configure imternet and try again");
+    if(matchPassword($pwd, $cpwd) !== false){
+        header ("location: ../index.php?error=unmatchedPassword");
+        exit();
     }
-
+    if(userExist($conn, $name, $email) !== false){
+        header ("location: ../index.php?error=userAlreadyexit");
+        exit();
+    }
+    createUser($conn, $name, $email, $pwd);
+}else{
+    header ("location: ../index.php");
+    exit();
 }

a668de883876b941372d391fa5310a802c5cc009
Dennis Kamau <73742235+denmau@users.noreply.github.com>
2021-06-30 21:07:53 +0300

    heroku database connection configuration

--- includes/conn.php
+++ includes/conn.php
@@ -1,8 +1,29 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'libsystem');
+// $conn = new mysqli('localhost', 'root', '', 'libsystem');
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
+// if ($conn->connect_error) {
+// die("Connection failed: " . $conn->connect_error);
+// }
+
+// This environment variable is necessary when working with Heroku Db
+// We will be using Heroku's Clear Database
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+// To use LocalDB configuration, uncomment next line, otherwise make sure its commented
+//$cleardb_url = null;
+
+$host = $cleardb_url ? $cleardb_url["host"] : "localhost";
+$user = $cleardb_url ? $cleardb_url["user"] : "root";
+$pass = $cleardb_url ? $cleardb_url["pass"] : "root";
+$dbname = $cleardb_url ? substr($cleardb_url["path"], 1) : "libsystem";
+
+try {
+	// Create connection
+	$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+	$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo $error_message = "Connection failed: " . $e->getMessage();
+	exit();
+}

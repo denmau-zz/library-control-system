3a72f40c1d0d3c0d5c63a88389dbe52714ff9a14
Dennis Kamau <73742235+denmau@users.noreply.github.com>
2021-07-01 08:43:06 +0300

    replace project homepage

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/alpha.css">
-    <title>linker</title>
+    <title>Library System</title>
     <link rel="shortcut icon" type="image/ico" href="images/favicon.ico">  
 </head>
 
